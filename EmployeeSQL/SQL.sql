c268027835107040e9703e0572d39d04171f6879
Rolando Navarro <rolandonavarro@Rolandos-MacBook-Pro.local>
2020-04-04 12:51:38 -0500

    SQL HW

--- EmployeeSQL/SQL.sql
+++ EmployeeSQL/SQL.sql
@@ -78,7 +78,6 @@ WHERE hire_date>'1986-01-01' AND hire_date<'1986-12-30';
 -- List the manager of each department with the following information: department number, department name, 
 -- the manager's employee number, last name, first name, and start and end employment dates.
 
-
 SELECT 
 d.dept_no, -- dept_manager
 f.dept_name, -- departments
@@ -88,25 +87,38 @@ e.first_name,
 d.from_date,
 d.to_date
 FROM 
-departments AS f, 
-dept_manager AS d, 
-employees AS E
-WHERE f.dept_no=d.dept_no
-AND d.emp_no=e.emp_no;
+departments AS f
+JOIN dept_manager AS d
+ON f.dept_no=d.dept_no
+JOIN employees AS e 
+ON d.emp_no=e.emp_no;
 
 
--- List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
 
 SELECT
+e.emp_no,
+e.last_name,
+e.first_name,
+d.dept_name
+FROM employees AS e
+JOIN dep_emp AS de
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no;
 
 
 -- List all employees whose first name is "Hercules" and last names begin with "B."
 
--- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- List all employees in the Sales department, including their 
+-- employee number, last name, first name, and department name.
 
--- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
 
--- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- In descending order, list the frequency count of employee last names, i.e., 
+-- how many employees share each last name.
 
 
 
